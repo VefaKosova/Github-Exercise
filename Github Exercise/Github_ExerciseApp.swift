4bda9ca5edd5c87809fe6c20b4e0407966318d00
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-08 19:47:21 +0300

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Vefa Kosova on 8.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
